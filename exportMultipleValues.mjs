8d9c51f912e6143f06efafd39bb8d221b6200eb1
Sai Sindu <saisindu9398640181@gmail.com>
2023-08-14 12:08:52 +0000

    Import and Export Multiple Values using ES6 Module Syntax

---
+++ exportMultipleValues.mjs
@@ -0,0 +1,9 @@
+let myArray = ["camel", 265, true, "5.6"];
+let bulb = {
+  watts: 10,
+  type: "LED",
+};
+let multiplyByFour = function (num) {
+  return num * 4;
+};
+export { myArray, bulb, multiplyByFour };
---
+++ importMultipleValues.mjs
@@ -0,0 +1,4 @@
+import { myArray, bulb, multiplyByFour } from "./exportMultipleValues.mjs";
+console.log(myArray);
+console.log(bulb);
+console.log(multiplyByFour(4));
